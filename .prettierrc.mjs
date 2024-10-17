4e668137d3e8397473f75025f4aaf0229e1f5e19
gusalbukrk <gusalbukrk@gmail.com>
2024-10-17 10:24:27 -0300

    add `prettier`

---
+++ .prettierrc.mjs
@@ -0,0 +1,7 @@
+// must add prettier as a project dependency, otherwise vscode prettier extension will error
+// https://github.com/prettier/prettier-vscode/issues/3066#issuecomment-1927442185
+
+/** @type {import("prettier").Config} */
+export default {
+  singleQuote: true,
+};
--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,17 @@
-import { Image, StyleSheet, Platform } from "react-native";
+import { Image, StyleSheet, Platform } from 'react-native';
 
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require("@/assets/images/arduino.png")}
+          source={require('@/assets/images/arduino.png')}
           style={styles.reactLogo}
           resizeMode="contain"
         />
@@ -35,8 +35,8 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
     gap: 8,
     marginBottom: 32,
   },
